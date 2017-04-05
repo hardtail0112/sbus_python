2e680331408f4b2fc1502f255bf68e17d9940ee8
hardtail0112 <takaaki0112@gmail.com>
2017-04-05 15:06:10 +0900

    add read, write and close function

--- sbus.c
+++ sbus.c
@@ -13,9 +13,28 @@
 #define BAUD_RATE    100000             // RS232C通信ボーレート
 #define BUFF_SIZE    4096               // 適当
 
+
+unsigned char buffer[BUFF_SIZE];    // データ受信バッファ
+int start_byte_flag;
+int sbus_bffer_cnt;
+int sbus_bit_cnt;
+int sbus_byte_cnt;
+unsigned char sbus_buffer[25];
+int sbus_channels[8];
+int gfd;
+
+//write
+unsigned char write_sbus_buffer[25];
+int sbus_write_channels[8];
+
+
+struct itimerval timer;
+
+void sbus_hander();
+
+
 // timer init
-void SignalHandler(int);
-int _nanosleep(int, int);
+#if 0
 void timer_init();
 
 void timer_init(){
@@ -36,50 +55,31 @@ void timer_init(){
 
     /* set intarval timer (20ms) */
     timer.it_value.tv_sec = 0;
-    timer.it_value.tv_usec = 20000;
+    timer.it_value.tv_usec = 25000;
     timer.it_interval.tv_sec = 0;
-    timer.it_interval.tv_usec = 20000;
+    timer.it_interval.tv_usec = 25000;
     if(setitimer(ITIMER_REAL, &timer, NULL) < 0){
         perror("setitimer error");
         exit(1);
     }
 }
-
-
-void SignalHandler(int signum)
-{
-    static unsigned long msec_cnt = 0;
-
-    msec_cnt++;
-    if(!(msec_cnt % 100)){
-        printf("SignalHandler:%lu sec\n", (msec_cnt / 100));
-    }
-    return;
-}
-int _nanosleep(int sec, int nsec)
-{
-    struct timespec req, rem;
-
-    req.tv_sec = sec;
-    req.tv_nsec = nsec;
-    rem.tv_sec = 0;
-    rem.tv_nsec = 0;
-    while(nanosleep(&req, &rem)){
-        if(errno == EINTR){
-            req.tv_sec = rem.tv_sec;
-            req.tv_nsec = rem.tv_nsec;
-        }else{
-            perror("nanosleep error");
-            return -1;
-        }
-    }
-    return 0;
-}
-
+#endif
 
 // シリアルポートの初期化
-int serial_init(int fd)
+void serial_init()
 {
+    int fd;
+    int len;
+    int buffer_end_flag = 1;
+
+    // デバイスファイル（シリアルポート）オープン
+    fd = open(DEV_NAME,O_RDWR);
+    if(fd<0){
+        // デバイスの open() に失敗したら
+        //perror(argv[1]);
+         exit(1);
+    }
+
     struct termios2 tio;
     memset(&tio,0,sizeof(tio));
     tio.c_cflag = CS8 | CLOCAL | CREAD | CSTOPB | PARODD| PARENB;
@@ -94,19 +94,65 @@ int serial_init(int fd)
     // デバイスに設定を行う
     //tcsetattr(fd,TCSANOW,&tio);
     ioctl(fd, TCSETS2, &tio);
-    return fd;
+
+///////////////////////////////////////
+    int i;
+    /*
+    while(buffer_end_flag){
+        len=read(gfd,buffer,BUFF_SIZE);
+        if(len<0){
+            //printf("%s: ERROR\n",argv[0]);
+            // read()が負を返したら何らかのI/Oエラー
+            perror("");
+            exit(2);
+        }
+        // read()が正を返したら受信データ数
+
+        // 受信したデータを 16進数形式で表示  
+
+        for(i=0; i<len; i++){
+            if(buffer[i] == 0x00){
+              buffer_end_flag=0;  
+            }
+        }
+    }
+    */
+#if 1
+    struct sigaction action;
+    //struct itimerval timer;
+
+    //printf("sample program(%s) start\n", __FILE__);
+    memset(&action, 0, sizeof(action));
+    //printf("memset finished\n");
+
+    /* set signal handler */
+    //action.sa_handler = SignalHandler;
+    action.sa_handler =  sbus_hander;
+    action.sa_flags = SA_RESTART;
+    sigemptyset(&action.sa_mask);
+    if(sigaction(SIGALRM, &action, NULL) < 0){
+        perror("sigaction error");
+        exit(1);
+    }
+   // printf("action timer set finished\n");
+
+    /* set intarval timer (10ms) */
+    timer.it_value.tv_sec = 0;
+    timer.it_value.tv_usec = 25000;
+    timer.it_interval.tv_sec = 0;
+    timer.it_interval.tv_usec = 25000;
+    if(setitimer(ITIMER_REAL, &timer, NULL) < 0){
+        perror("setitimer error");
+        exit(1);
+    }
+    //printf("timer set finished\n");
+#endif
+
+    gfd = fd;
+    //printf("gfd set finished\n");
 }
 
-unsigned char buffer[BUFF_SIZE];    // データ受信バッファ
-int start_byte_flag;
-int sbus_bffer_cnt;
-int sbus_bit_cnt;
-int sbus_byte_cnt;
-unsigned char sbus_buffer[25];
-int sbus_channels[8];
-int gfd;
-
-void read_sbus(){
+void sbus_hander(){
     int i,j;
     int len;                            //  受信データ数（バイト）
     
@@ -165,14 +211,66 @@ void read_sbus(){
         //printf("%02X ",buffer[i]);
     }
 
+    //write sequence
+    
+    write_sbus_buffer[0] = 0x0f;
+
+    write_sbus_buffer[ 1] =   sbus_write_channels[0] & 0b00011111111;
+    write_sbus_buffer[ 2] = ((sbus_write_channels[1] & 0b00000011111)<<3) | ((sbus_write_channels[0] & 0b11100000000)>>8);
+    write_sbus_buffer[ 3] = ((sbus_write_channels[2] & 0b00000000011)<<6) | ((sbus_write_channels[1] & 0b11111100000)>>5);
+    write_sbus_buffer[ 4] = ((sbus_write_channels[2] & 0b01111111100)>>2);
+    write_sbus_buffer[ 5] = ((sbus_write_channels[3] & 0b00001111111)<<1) | ((sbus_write_channels[2] & 0b10000000000)>>10);
+    write_sbus_buffer[ 6] = ((sbus_write_channels[4] & 0b00000000111)<<4) | ((sbus_write_channels[3] & 0b11110000000)>>7);
+    write_sbus_buffer[ 7] = ((sbus_write_channels[4] & 0b11111111000)>>3);
+    write_sbus_buffer[ 8] =   sbus_write_channels[5] & 0b00011111111;
+    write_sbus_buffer[ 9] = ((sbus_write_channels[6] & 0b00000011111)<<3) | ((sbus_write_channels[5] & 0b11100000000)>>8);
+    write_sbus_buffer[10] = ((sbus_write_channels[7] & 0b00000000011)<<6) | ((sbus_write_channels[6] & 0b11111100000)>>5);
+    write_sbus_buffer[11] = ((sbus_write_channels[7] & 0b01111111100)>>2);
+
+
+    for(i=12; i<=26; i++){
+        write_sbus_buffer[i] = sbus_buffer[i];
+    }
+
+    write(gfd, write_sbus_buffer, 25);
     //return sbus_channels;        
+}
+
+void read_sbus(int *b){
+    int j;
+    //_nanosleep(0, 200000000);    /* sleep 1 sec */
+    for(j=1;j<9;j++){
+        //printf("%02X ",sbus_buffer[j]);
+        //printf("ch%d:%5d  ",j, sbus_channels[j-1]);
+        b[j-1] = sbus_channels[j-1];
+    }
+    //printf("\n"); 
+}
+
+void serial_close(){
+    timer.it_value.tv_usec = 0;
+    timer.it_interval.tv_usec = 0;
+    
+    if(setitimer(ITIMER_REAL, &timer, NULL) < 0){
+        perror("setitimer error");
+        exit(1);
+    }
+
+    close(gfd);
+}
+
+void write_sbus(int *b){
+    int i;
+    for(i=0;i<8;i++){
+        sbus_write_channels[i] = b[i];
+    }
 }
 
 
 /* --------------------------------------------------------------------- */
 /* メイン                                                                */
 /* --------------------------------------------------------------------- */
-
+#if 0
 int main(int argc,char *argv[]){
     int fd;
 
@@ -184,43 +282,10 @@ int main(int argc,char *argv[]){
     unsigned char sbus_buffer[25];
     //int sbus_channels[8];
 
-    // デバイスファイル（シリアルポート）オープン
-    fd = open(DEV_NAME,O_RDWR);
-    if(fd<0){
-        // デバイスの open() に失敗したら
-        perror(argv[1]);
-         exit(1);
-    }
+    printf("befor init \n");
 
     // シリアルポートの初期化
     gfd =  serial_init(fd);
-
-
-    struct sigaction action;
-    struct itimerval timer;
-
-    printf("sample program(%s) start\n", __FILE__);
-    memset(&action, 0, sizeof(action));
-
-    /* set signal handler */
-    //action.sa_handler = SignalHandler;
-    action.sa_handler =  read_sbus;
-    action.sa_flags = SA_RESTART;
-    sigemptyset(&action.sa_mask);
-    if(sigaction(SIGALRM, &action, NULL) < 0){
-        perror("sigaction error");
-        exit(1);
-    }
-
-    /* set intarval timer (10ms) */
-    timer.it_value.tv_sec = 0;
-    timer.it_value.tv_usec = 20000;
-    timer.it_interval.tv_sec = 0;
-    timer.it_interval.tv_usec = 20000;
-    if(setitimer(ITIMER_REAL, &timer, NULL) < 0){
-        perror("setitimer error");
-        exit(1);
-    }
 
     printf("loop start\n");
 
@@ -228,7 +293,7 @@ int main(int argc,char *argv[]){
     while(1){
         int j;
 
-        _nanosleep(0, 200000000);    /* sleep 1 sec */
+        //_nanosleep(0, 200000000);    /* sleep 1 sec */
         for(j=1;j<9;j++){
             //printf("%02X ",sbus_buffer[j]);
             printf("ch%d:%5d  ",j, sbus_channels[j-1]);
@@ -236,3 +301,4 @@ int main(int argc,char *argv[]){
         printf("\n"); 
     }
 }
+#endif
